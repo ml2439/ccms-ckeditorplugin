62f3fa804e9cd069f4565a8147b359f5c995fbad
Harry Moore <harrymoore@yahoo.com>
2017-10-12 18:17:16 -0400

    path updates

--- ouphello/ouphello/helper.js
+++ ouphello/ouphello/helper.js
@@ -10,7 +10,7 @@ define(function(require, exports, module) {
     return {
         registerPlugin: function(pluginId, moduleId)
         {
-            var pluginPath = "../../../" + moduleId + "/" + pluginId + "/";
+            var pluginPath = "../../../" + moduleId + "/" + moduleId + "/" + pluginId + "/";
 
             CKEDITOR.plugins.addExternal(pluginId, pluginPath, 'plugin.js');
             // CKEDITOR.config.customConfig = pluginPath + "../ckeditor_config.js";
